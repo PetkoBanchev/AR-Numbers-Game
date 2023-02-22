b081b469b89c6a5319a3e731c3709a1876b7c509
P_Banchev <petkobanchev1122@gmail.com>
2023-02-22 16:10:08 +0100

    Simple object spawner added

---
+++ AR Numbers Game/Assets/Scripts/ObjectSpawner.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+
+[RequireComponent(typeof(ARRaycastManager))]
+public class ObjectSpawner : MonoBehaviour
+{
+    private ARRaycastManager raycastManager;
+    private GameObject spawnedObject;
+
+    [SerializeField]
+    private GameObject prefab; //to be renamed
+
+    private static List<ARRaycastHit> s_Hits = new List<ARRaycastHit>();
+
+    private void Awake()
+    {
+        raycastManager = GetComponent<ARRaycastManager>();  
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(!TryGetTouchPosition(out Vector2 touchPosition))
+        {
+            return;
+        }
+        if(raycastManager.Raycast(touchPosition, s_Hits, TrackableType.PlaneWithinPolygon))
+        {
+            var hitPose = s_Hits[0].pose;
+            if(spawnedObject == null)
+            {
+                spawnedObject = Instantiate(prefab, hitPose.position, hitPose.rotation);
+            }
+            else
+            {
+                spawnedObject.transform.position = hitPose.position;
+                spawnedObject.transform.rotation= hitPose.rotation;
+            }
+        }
+    }
+
+    private bool TryGetTouchPosition(out Vector2 touchPosition)
+    {
+        if(Input.touchCount > 0)
+        {
+            touchPosition = Input.GetTouch(0).position;
+            return true;
+        }
+        touchPosition = default;
+        return false;   
+    }
+}
